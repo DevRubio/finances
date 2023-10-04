4225b87d7b8a49735acc4691b0b37c748d4c3e3d
oscar.franco <ofrubio0@gmail.com>
2023-10-04 07:26:53 -0500

    (ADD) Se instalan dependencias de Datapicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ const colors = require('tailwindcss/colors')
 export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js"
   ],
   theme: {},
   plugins: [
