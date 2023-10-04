21ed93a504f7b2098603bb0f20ae47e1619c2166
oscar.franco <ofrubio0@gmail.com>
2023-10-04 07:33:30 -0500

    (UPDATE) Se agregan estilos a los componentes CardAccounts y Navbar

--- src/components/CardAccounts/index.jsx
+++ src/components/CardAccounts/index.jsx
@@ -1,10 +1,10 @@
+import { IconWallet } from "../../assets/Icons";
 const CardAccounts = (props) => {
   return (
-    <div className={`flex ${props.color} rounded-lg cursor-pointer`}>
-        {console.log(props.color)}
-      <div className="w-20 flex justify-center items-center">🪙</div>
+    <div className={`flex ${props.color} rounded-lg cursor-pointer`}>       
+      <div className="w-20 flex justify-center items-center"><IconWallet/></div>
       <div className="w-full flex items-start flex-col">
-        <span className="text-gray-300 font-medium mt-1">{props.name}</span>
+        <span className="text-gray-100 font-medium mt-1">{props.name}</span>
         <p className="text-white font-semibold mb-1">{props.balance} COP</p>
       </div>
     </div>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,9 @@
 const Navbar = () =>{
     return(
-        <div className="flex border w-full h-full">
-            <div className="flex list-none items-center justify-between gap-3">
-                <li className="font-semibold text-lg">Panel de Control</li>
-                <li>Cuentas</li>
+        <div className="flex border w-full h-20">
+            <div className="flex ml-16 list-none items-center justify-between gap-3">
+                <li className="font-semibold text-lg cursor-pointer">Panel de Control</li>
+                <li className="cursor-pointer">Cuentas</li>
             </div>
         </div>
     )
