08e842eed362a5d64b9e886fd783fe0aee88edd9
oscar.franco <ofrubio0@gmail.com>
2023-10-04 07:30:33 -0500

    (UPDATE) Se listan las accounts desde App y se pasan a travez de props al componente Accounts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import './App.css'
+//import './App.css'
 import { Navbar } from './components/Navbar'
 import { Accounts } from './components/Accounts'
+import { AddRecords } from './components/AddRecords'
 
 function App() {
+
+  const acounts = [
+    {
+        name: "Efectivo",
+        balance: "500.000",
+        color: "bg-[#1fb6ff]"
+    },
+    {
+        name: "Banco 1",
+        balance: "256.450",
+        color:"bg-[#ffb300ff]"
+    },
+    {
+        name: "Banco 2",
+        balance: "356.450",
+        color:'bg-[#6a1b9a]'
+    }        ,
+    {
+        name: "Banco 3",
+        balance: "356.450",
+        color:'bg-[#d32f2fff]'
+    }
+]
 
   return (
     <>
       <Navbar/>
-      <Accounts/>
+      <Accounts 
+      acounts={acounts}
+      />
+      <AddRecords
+      acounts={acounts}
+      />
     </>
   )
 }
--- src/components/Accounts/index.jsx
+++ src/components/Accounts/index.jsx
@@ -1,45 +1,21 @@
-import {CardAccounts} from '../CardAccounts/index'
-import { AddAccount } from '../AddAccount'
-const Accounts = () =>{
+import { CardAccounts } from "../CardAccounts/index";
+import { AddAccount } from "../AddAccount";
+const Accounts = ({ acounts }) => {
+  return (
+    <div className="w-full bg-slate-100 h-full flex flex-col">
+      <div className="grid mt-5 ml-16 mb-8 mr-16 grid-cols-3 gap-2">
+        {acounts.map((item) => (
+          <CardAccounts
+            color={item.color}
+            key={item.name}
+            name={item.name}
+            balance={item.balance}
+          />
+        ))}
+        <AddAccount />
+      </div>
+    </div>
+  );
+};
 
-    const acounts = [
-        {
-            name: "Efectivo",
-            balance: "500.000",
-            color: "bg-[#1fb6ff]"
-        },
-        {
-            name: "Banco 1",
-            balance: "256.450",
-            color:"bg-[#ffb300ff]"
-        },
-        {
-            name: "Banco 2",
-            balance: "356.450",
-            color:'bg-[#6a1b9a]'
-        }        ,
-        {
-            name: "Banco 3",
-            balance: "356.450",
-            color:'bg-[#d32f2fff]'
-        }
-    ]
-
-    return(
-        <div className="grid mt-5 grid-cols-3 gap-2">
-            {acounts.map(item=>(
-                
-                <CardAccounts                    
-                    color={item.color}
-                    key={item.name}
-                    name={item.name}
-                    balance={item.balance}                    
-                />
-                ))
-            }
-            <AddAccount/>
-        </div>
-    )
-}
-
-export { Accounts }
+export { Accounts };
